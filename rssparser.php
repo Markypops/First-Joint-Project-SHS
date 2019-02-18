6554099799026797c73dc84c9b625098f841e177
MinjeongMark <45309942+MinjeongMark@users.noreply.github.com>
2019-02-18 17:14:56 +0800

    Semi-Final

--- rssparser.php
+++ rssparser.php
@@ -19,41 +19,6 @@ $xml = simplexml_load_file($url);
 
         $html .= "<div><h2>$stitle</h2><h3>$sdesc</h3><br><a href='$slink' target='_blank'>$slink</a></div>";
 
-        $sql = "INSERT INTO tbl_news_fragments(title,link,description) values('$stitle', '$slink','$sdesc')";
-        $result = $conn->query($sql);
-    
-        if($result === TRUE)
-        {
-            echo "Created Successfully!<br>";
-        }
-        else
-        {
-            echo $conn->error ."<br>";
-        }
-    }
-
-echo $html;
-
-<?php
-include 'conndb.php';
-$html = "";
-$url = $_POST['url'];
-$xml = simplexml_load_file($url);
-
-    for($i = 0;$i < 10;$i++)
-    {
-        $title = $xml->channel->item[$i]->title;
-        $link = $xml->channel->item[$i]->link;
-        $desc = $xml->channel->item[$i]->description;
-        $etitle = mysqli_escape_string($conn, $title); 
-        $elink = mysqli_escape_string($conn, $link);
-        $edesc = mysqli_escape_string($conn, $desc);
-        $stitle = stripslashes($etitle); 
-        $slink = stripslashes($elink);
-        $sdesc = stripslashes($edesc);
-
-        $html .= "<div><h2>$stitle</h2><h3>$sdesc</h3><br><a href='$slink' target='_blank'>$slink</a></div>";
-
         $sql = "INSERT INTO tbl_news_fragments(title,link,description) values('$etitle', '$elink','$edesc')";
         $result = $conn->query($sql);
     
@@ -68,4 +33,5 @@ $xml = simplexml_load_file($url);
     }
 
 echo $html;
+
 ?>
