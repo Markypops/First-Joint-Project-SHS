694d8a55a41aea7b1dd6a3cd149e9d4cc83a0f94
MinjeongMark <45309942+MinjeongMark@users.noreply.github.com>
2019-02-18 16:40:23 +0800

    V3
    **FIXED DATABASE INPUT ERROR**
    (Probably)

--- rssparser.php
+++ rssparser.php
@@ -1,19 +1,3 @@
-
-<html>
-<body>
-    <center>
-
-        <h1> RSS IMMERSION </h1>
-    <form action="rssparser.php" method = "POST">
-    <input type="text" name = "url" placeholder = "Paste RSS URL" autocomplete = "OFF">
-    <input type="submit" value = "Submit">
-    
-    </form>
-    <br>
-    <br>
-    </center>
-</body>
-
 <?php
 include 'conndb.php';
 $html = "";
@@ -46,72 +30,6 @@ $xml = simplexml_load_file($url);
             echo $conn->error ."<br>";
         }
     }
+
 echo $html;
 ?>
-=======
-<html>
-<body>
-    <center>
-
-        <h1> RSS IMMERSION </h1>
-    <form action="rssparser.php" method = "POST">
-    <input type="text" name = "url" placeholder = "Paste RSS URL" autocomplete = "OFF">
-    <input type="submit" value = "Submit">
-    
-    </form>
-    <br>
-    <br>
-    </center>
-</body>
-
-<?php
-include 'conndb.php';
-$html = "";
-$url = $_POST['url'];
-$xml = simplexml_load_file($url);
-
-
-    for($i = 0;$i < 10;$i++)
-    {
-        $title = $xml->channel->item[$i]->title;
-        $link = $xml->channel->item[$i]->link;
-        $desc = $xml->channel->item[$i]->description;
-        $html .= "<div><h2>$title</h2><h3>$desc</h3><br><a href='$link' target='_blank'>$link</a></div>";
-
-        $sql = "INSERT INTO tbl_news_fragments(title,link,description) values('$title', '$link','$desc')";
-        $result = $conn->query($sql);
-    
-    }
-echo $html;
-?>
-<?php
-include 'conndb.php';
-
-$sql = "SELECT * FROM tbl_news_fragments";
-$result=$conn->query($sql);
-
-if($result->num_rows>0)
-{
-    while($row=$result->fetch_assoc())
-    {
-        $title = $row['title'];
-        $link = $row['link'];
-        $desc = $row['description'];
-
-    }
-}
-else
-{
-    echo"Error 408: Login information not found";
-}
-
-    echo ("<html>
-    <body>
-        <p>$title <br></p>
-        $desc<br>
-        $link<br>
-    
-    </body>
-    </html>");
-?>
-
