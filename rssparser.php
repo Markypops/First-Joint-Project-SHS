5d060d2d944fdff9b47c8de1f593a27804e02962
Hannah Mae Hormiguera <47653750+Maennah@users.noreply.github.com>
2019-02-18 09:41:23 +0700

    Add files via upload

--- rssparser.php
+++ rssparser.php
@@ -30,15 +30,37 @@ $xml = simplexml_load_file($url);
         $sql = "INSERT INTO tbl_news_fragments(title,link,description) values('$title', '$link','$desc')";
         $result = $conn->query($sql);
     
-        if($result === TRUE)
-        {
-            echo "Created Successfully!<br>";
-        }
-        else
-        {
-            echo $conn->error ."<br>";
-        }
-    
     }
 echo $html;
 ?>
+<?php
+include 'conndb.php';
+
+$sql = "SELECT * FROM tbl_news_fragments";
+$result=$conn->query($sql);
+
+if($result->num_rows>0)
+{
+    while($row=$result->fetch_assoc())
+    {
+        $title = $row['title'];
+        $link = $row['link'];
+        $desc = $row['description'];
+
+    }
+}
+else
+{
+    echo"Error 408: Login information not found";
+}
+
+    echo ("<html>
+    <body>
+        <p>$title <br></p>
+        $desc<br>
+        $link<br>
+    
+    </body>
+    </html>");
+?>
+
--- searchbar.php
+++ searchbar.php
@@ -2,13 +2,22 @@
 
 <body>
     <center>
+        <div class="container">
+ <img src="img.jpg" alt="Snow" height="1000px" width="1720px">
+  <div class="bottom-left"></div>
+  <div class="top-left"></div>
+  <div class="top-right"></div>
+  <div class="bottom-right"></div>
+  <div class="centered"></div>
+</div>
+class
+        
          <br> <br> <br>
         <h1> RSS IMMERSION </h1>
         <br> <br> <br>
     <form action="rssparser.php" method = "POST">
     <input type="text" name = "url" placeholder = "Paste RSS URL" autocomplete = "OFF">
     <input type="submit" value = "Submit">
-    
     </form>
     <br>
     <br>
@@ -16,37 +25,4 @@
 </body>
 
 
-
 </html>
-
-<?php
-include 'conndb.php';
-
-$sql = "SELECT * FROM tbl_news_fragments";
-$result=$conn->query($sql);
-
-if($result->num_rows>0)
-{
-	while($row=$result->fetch_assoc())
-	{
-		$title = $row['title'];
-		$link = $row['link'];
-		$desc = $row['description'];
-
-	}
-}
-else
-{
-	echo"Error 408: Login information not found";
-}
-
-    echo ("<html>
-    <body>
-        <p>$title <br></p>
-        $desc<br>
-        $link<br>
-    
-    </body>
-    </html>");
-?>
-
